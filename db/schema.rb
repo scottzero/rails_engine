308b8244fba60694949da12a925073fc6d6eed9a
Scott Payton <spayton.c@gmail.com>
2019-10-01 01:15:00 -0600

    Add invoice to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191001065141) do
+ActiveRecord::Schema.define(version: 20191001070031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,9 +61,12 @@ ActiveRecord::Schema.define(version: 20191001065141) do
     t.string "result"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
