ea1fd0076f08ad3c3edb392bad6a95fda430266c
Scott Payton <spayton.c@gmail.com>
2019-10-01 17:49:57 -0600

    Add endpoints for each data set

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :invoice_items, except: [:new, :edit]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :invoices, except: [:new, :edit]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, except: [:new, :edit]
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :customers, except: [:new, :edit]
+    end
+  end
+
   namespace :api do
     namespace :v1 do
     resources :merchants, except: [:new,:edit]
