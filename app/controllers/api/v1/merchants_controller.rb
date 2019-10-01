db0ec4ad3088c577c20dc68af752814a56c02c23
Scott Payton <spayton.c@gmail.com>
2019-10-01 16:41:13 -0600

    Add strong params to pass for serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,14 @@
 # app/controllers/api/v1/merchants_controller.rb
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.where(merchant_params))
+  end
+
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 
 end
