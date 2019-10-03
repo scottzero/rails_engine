d3b5cbfcadef5fbe40fdffe2f504ff7025c5e956
Scott Payton <spayton.c@gmail.com>
2019-10-03 09:59:56 -0600

    created controller for customer search

---
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Customers::SearchController < ApplicationController
+
+  def show
+    render json: CustomerSerializer.new(Customer.find_by(customer_params))
+  end
+
+
+  private
+
+  def customer_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+  end
+
+end
