b4ea7beb726ee9637ccffba6796d7201dff0a8b0
Scott Payton <spayton.c@gmail.com>
2019-10-03 09:53:43 -0600

    Created search controller for invoice items

---
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::InvoiceItems::SearchController < ApplicationController
+
+  def show
+    render json: InvoiceItemSerializer.new(InvoiceItem.find_by(invoice_item_params))
+  end
+
+
+  private
+
+  def invoice_item_params
+    params.permit(:id, :quantity, :unit_price, :item_id, :invoice_id, :created_at, :updated_at)
+  end
+
+end
