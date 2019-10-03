a5e3c5910015a775a34250589b10c179a64e32d2
Scott Payton <spayton.c@gmail.com>
2019-10-03 09:42:43 -0600

    create spec and controller for single finders by transaction

---
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Transactions::SearchController < ApplicationController
+
+  def show
+    render json: TransactionSerializer.new(Transaction.find_by(transaction_params))
+  end
+
+
+  private
+
+  def transaction_params
+    params.permit(:id, :credit_card_number, :result, :invoice_id, :created_at, :updated_at)
+  end
+
+end
