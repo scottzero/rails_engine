4e6418793a73f1d443797664c47f019e33a8d10b
Scott Payton <spayton.c@gmail.com>
2019-10-03 09:59:20 -0600

    Added namespace for customers to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
   end
 end
 
-
-
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index, :show]
+      namespace :customers do
+        get "/find", to: "search#show"
     end
+    resources :customers, only: [:index, :show]
   end
-
+end
 
   namespace :api do
     namespace :v1 do
