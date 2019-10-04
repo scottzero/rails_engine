5c816b03c829ecf90565b00d998050f405e6c083
Scott Payton <spayton.c@gmail.com>
2019-10-03 19:31:50 -0600

    Created index action for merchant multi finders

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
+
+  def index
+    # pry
+    render json: MerchantSerializer.new(Merchant.where(merchant_params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
   end
