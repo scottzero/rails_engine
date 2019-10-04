f9b418dae8d600198153dcc5e425fb038747a604
Scott Payton <spayton.c@gmail.com>
2019-10-03 19:32:10 -0600

    Created end point for merchant find all for multi finders

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ end
     namespace :v1 do
       namespace :merchants do
       get "/find", to: "search#show"
+      get "/find_all", to: "search#index"
     end
     resources :merchants, only: [:index,:show]
   end
