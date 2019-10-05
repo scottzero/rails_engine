117db4d0bacb86ad6010331c2bb5f6ea89e5ef5f
Scott Payton <spayton.c@gmail.com>
2019-10-05 01:33:45 -0600

    add business logic to merchant model most rev

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,15 +2,4 @@ class Merchant < ApplicationRecord
 has_many :invoices
 has_many :items
 validates_presence_of :name
-
-
-def self.most_revenue(x)
-  select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price)AS revenue")
-  .joins(invoices: [:invoice_items, :transactions])
-  .where(transactions: {result: "success"})
-  .group(:id)
-  .order("revenue DESC")
-  .limit(x)
 end
-
-end 
