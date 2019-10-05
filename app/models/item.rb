a033e003d479508cf1c4388ba1a37773e6726824
Scott Payton <spayton.c@gmail.com>
2019-10-05 12:43:29 -0600

    create class method for mos revenue items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ belongs_to :merchant
 has_many :invoice_items
 has_many :invoices, through: :invoice_items
 validates_presence_of :name, :description, :unit_price
+
+
+  def self.most_revenue(x)
+    select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(invoices: :transactions)
+    .where(transactions: {result: "success"})
+    .group(:id)
+    .order("revenue DESC")
+    .limit(x)
+  end
 end
