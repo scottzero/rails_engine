f943495eb4f1b1cce0ca41b7a894f0499baa9bf8
Scott Payton <spayton.c@gmail.com>
2019-10-04 21:08:32 -0600

    created item relationship spec for merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -97,4 +97,18 @@ end
     expect(ids).to include(merchant["id"].to_i)
   end
   end
+
+  context "relationship endpoints" do
+    it "can get all items for a single merchant" do
+      merchant = create(:merchant)
+      item_1 = create(:item, merchant: merchant)
+      item_2 = create(:item, merchant: merchant)
+      item_3 = create(:item, merchant: merchant)
+      get "/api/v1/merchants/#{merchant.id}/items"
+      expect(response).to be_successful
+      merchant_items = JSON.parse(response.body)["data"]
+      expect(merchant_items[0]["id"].to_i).to eq(item_1.id)
+      expect(merchant_items[1]["id"].to_i).to eq(item_2.id)
+      expect(merchant_items[2]["id"].to_i).to eq(item_3.id)
+    end
 end
