f83bbf3dfd125d995db8dda76dcbea6856c22b19
Scott Payton <spayton.c@gmail.com>
2019-10-05 17:00:04 -0600

    attempt to fix customer transaction controller, still buggy

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::Customers::TransactionsController < ApplicationController
   def index
     #need to identify customer FIRST,  instead of nested function
     customer = Customer.find[params[:id]]
+    pry
     render json: TransactionSerializer.new(customer.transactions)
   end
 end
