86546660cd9a4c8a5a97feebac52d162c0e1f454
Scott Payton <spayton.c@gmail.com>
2019-10-05 17:01:09 -0600

    refactor invoices controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,16 +1,10 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
-    render json: InvoiceSerializer.new(Invoice.where(invoice_params))
+    render json: InvoiceSerializer.new(Invoice.all)
   end
 
   def show
     render json: InvoiceSerializer.new(Invoice.find(params[:id]))
-  end
-
-  private
-
-  def invoice_params
-    params.permit(:id, :status, :merchant_id, :customer_id, :created_at, :updated_at)
   end
 
 end
