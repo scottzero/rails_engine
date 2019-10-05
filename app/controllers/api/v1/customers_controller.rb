02392dc2f556c0ca412239f55936c419c9da5c11
Scott Payton <spayton.c@gmail.com>
2019-10-05 17:00:27 -0600

    refactor customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,16 +1,9 @@
 class Api::V1::CustomersController < ApplicationController
   def index
-    render json: CustomerSerializer.new(Customer.where(customer_params))
+    render json: CustomerSerializer.new(Customer.all)
   end
 
   def show
     render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
-
-  private
-
-  def customer_params
-    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
-  end
-
 end
