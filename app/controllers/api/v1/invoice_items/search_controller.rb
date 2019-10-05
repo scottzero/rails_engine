969b56da7c812089b4d4dd95600641f5a28ae863
Scott Payton <spayton.c@gmail.com>
2019-10-04 22:21:18 -0600

    fix invoice search controller

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
     render json: InvoiceItemSerializer.new(InvoiceItem.find_by(invoice_item_params))
   end
 
+  def index
+    render json: InvoiceItemSerializer.new(InvoiceItem.where(invoice_item_params))
+  end
+
 
   private
 
