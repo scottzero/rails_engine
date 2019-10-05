f9ce5c8c0f89e9cc7bea3dc67c4db84de44aba41
Scott Payton <spayton.c@gmail.com>
2019-10-05 17:27:26 -0600

    fixed invoice item serializer to convert integer to sting floats

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,9 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :quantity, :unit_price, :item_id, :invoice_id
+
+  attribute :unit_price do |invoice_item|
+    invoice_item.unit_price = (invoice_item.unit_price.to_f/100).to_s
+  end
+
 end
