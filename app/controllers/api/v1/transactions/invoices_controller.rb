879194c2f2d5be393c5d00846650e74ed04a4f94
Scott Payton <spayton.c@gmail.com>
2019-10-06 14:09:34 -0600

    refactor invoices controller

--- app/controllers/api/v1/transactions/invoices_controller.rb
+++ app/controllers/api/v1/transactions/invoices_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::Transactions::InvoicesController < ApplicationController
   def show
     #off by 1 in the spec harness and I dont know why, fix later
     transaction = Transaction.find(params[:id])
+    # pry
     render json: InvoiceSerializer.new(transaction.invoice)
+
   end
 end
