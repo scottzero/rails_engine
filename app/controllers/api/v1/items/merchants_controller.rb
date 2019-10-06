2df79fc4d2a8a71d7a2f49c9afc6ca74023ecc5e
Scott Payton <spayton.c@gmail.com>
2019-10-06 14:11:10 -0600

    still trying to fix items merchant controller

--- app/controllers/api/v1/items/merchants_controller.rb
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Items::MerchantsController < ApplicationController
 
 
-  def index
-    render json: Merchant.new(Item.find(params[:id]).merchant)
+  def show
+    item = Item.find(params[:id])
+    render json: MerchantSerializer.new(item.merchant)
   end
 end
