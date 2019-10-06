b92ac98092fc4730171269d93d5b01b3c102150a
Scott Payton <spayton.c@gmail.com>
2019-10-06 14:44:04 -0600

    Need to render data..attributes..id to an integer..fix

--- app/controllers/api/v1/customers/favorite_merchant_controller.rb
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::Customers::FavoriteMerchantController < ApplicationController
 
   def show
     customer = Customer.find(params[:id])
-    render json: MerchantSerializer.new(customer.fave_merch)
+    #need to convert first ID into an integer. the one under [data]
+    #postman /customers/3/favoriter_merchant
+    render json: FaveMerchantSerializer.new(customer.fave_merch)
   end
 end
